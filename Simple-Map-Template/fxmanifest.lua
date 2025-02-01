229bd71e509113d402c112572051fa7b4cd5ba0f
GlueGunStanley <kasonstanley@gmail.com>
2025-01-31 22:29:10 -0500

    Add .YMF directory

---
+++ Simple-Map-Template/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Your Name'
+description 'A template for a simple FiveM map resource'
+
+files {
+    'stream/ytyp/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST'               'stream/ytyp/example.ytyp'
