3b809c6a1a26326ee7c4d012267171e5052c37d7
GlueGunStanley <kasonstanley@gmail.com>
2025-01-31 22:45:56 -0500

    Create NUI-Template

---
+++ NUI-Template/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Your Name'
+description 'A simple template for a FiveM NUI resource'
+
+dependencies {
+    'resource_name'
+}
+
+ui_page 'nui/index.html'
+
+files {
+    'nui/index.html',
+    'nui/css/style.css',
+    'nui/js/script.js',
+    'nui/assets/images/*.png',
+    'nui/assets/images/*.jpg',
+    'nui/assets/images/*.svg',
+    'nui/assets/images/*.webp'
+}
+
+shared_scripts {
+    'shared/sh_main.lua'
+}
+
+client_scripts {
+    'client/cl_main.lua'
+}
+
+server_scripts {
+    'server/sv_main.lua'
+}
+
+export {
+    'YourClientFunction'
+}
+
+server_exports { 
+    'YourServerFunction'
+}
