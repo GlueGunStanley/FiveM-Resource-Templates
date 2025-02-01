58c7304832d0d62b6b4800d8948405a79c41fa83
GlueGunStanley <kasonstanley@gmail.com>
2025-01-31 20:14:34 -0500

    Create Advanced-Map-Template

---
+++ Advanced-Map-Template/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Your Name'
+description 'A simple template for a FiveM map resource'
+
+files {
+    'data/meta/*.meta',
+    'data/xml/*.xml',
+    'stream/ytyp/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST'             'stream/ytyp/example.ytyp'
