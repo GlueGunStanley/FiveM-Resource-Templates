5acf5dd7ad2a4aed6c54367cf8722102045fdf6e
GlueGunStanley <kasonstanley@gmail.com>
2025-01-31 22:36:14 -0500

    Create Weapon-Template

---
+++ Weapon-Template/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'A template for a FiveM weapon resource'
+
+files {
+    'data/**/*.meta'
+}
+
+data_file 'WEAPON_METADATA_FILE'            'data/**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE'          'data/**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE'            'data/**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE'                 'data/**/weapon.meta'
